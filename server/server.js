e2569914c9610a2dd8109a1722b3ac40713b68a3
artiseni <artiseni95@qmail.id>
2021-08-09 18:28:28 +0700

    server express

--- server/server.js
+++
@@ -1,2 +0,0 @@
-const bad_word = 'cinta'
-console.log(`Selamat tidur, ${bed_word}`)
---
+++ server/src/server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+require('dotenv/config')
+
+const app = express()
+app.use(express.json())
+const port = process.env.SERVER_PORT
+
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}`)
+})
